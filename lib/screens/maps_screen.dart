70ca089333b66a0e5473f0d5c4ca3ee962955fcc
LL-Etiane <lukongleinyuyetiane@gmail.com>
2023-07-20 04:27:05 +0100

    feat: updates the map screen

--- lib/screens/maps_screen.dart
+++ lib/screens/maps_screen.dart
@@ -60,46 +60,88 @@ class _MapsScreenState extends State<MapsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: currentLocation == null
-          ? const Center(child: Text("Loading"))
-          : GoogleMap(
-              initialCameraPosition: CameraPosition(
-                target: LatLng(
-                    currentLocation!.latitude!, currentLocation!.longitude!),
-                zoom: 15,
-              ),
-              markers: {
-                Marker(
-                    markerId: const MarkerId('Current Location'),
-                    position: LatLng(currentLocation!.latitude!,
-                        currentLocation!.longitude!),
-                    infoWindow: InfoWindow(title: "Your location")),
-                Marker(
-                  markerId: const MarkerId('Source'),
-                  position: garageLocation,
-                  infoWindow: InfoWindow(title: "Garage"),
-                  icon: BitmapDescriptor.defaultMarkerWithHue(
-                      BitmapDescriptor.hueBlue),
+      appBar: AppBar(title: const Text('Garage Location')),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            child: currentLocation == null
+                ? const Center(child: Text("Loading"))
+                : GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: LatLng(currentLocation!.latitude!,
+                          currentLocation!.longitude!),
+                      zoom: 15,
+                    ),
+                    markers: {
+                      Marker(
+                          markerId: const MarkerId('Current Location'),
+                          position: LatLng(currentLocation!.latitude!,
+                              currentLocation!.longitude!),
+                          infoWindow: InfoWindow(title: "Your location")),
+                      Marker(
+                        markerId: const MarkerId('Source'),
+                        position: garageLocation,
+                        infoWindow: InfoWindow(title: "Garage"),
+                        icon: BitmapDescriptor.defaultMarkerWithHue(
+                            BitmapDescriptor.hueBlue),
+                      ),
+                    },
+                    polylines: {
+                      Polyline(
+                        polylineId: PolylineId('line'),
+                        points: [
+                          garageLocation,
+                          currentLocation == null
+                              ? washingPointLocation
+                              : LatLng(currentLocation!.latitude!,
+                                  currentLocation!.longitude!),
+                        ],
+                        width: 5,
+                        color: kPrimaryColor,
+                      )
+                    },
+                    onMapCreated: (GoogleMapController controller) {
+                      _controller.complete(controller);
+                    },
+                  ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/image/venza3.png', // replace with your image path
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    height: double.infinity,
+                  ),
                 ),
-              },
-              polylines: {
-                Polyline(
-                  polylineId: PolylineId('line'),
-                  points: [
-                    garageLocation,
-                    currentLocation == null
-                        ? washingPointLocation
-                        : LatLng(currentLocation!.latitude!,
-                            currentLocation!.longitude!),
-                  ],
-                  width: 5,
-                  color: kPrimaryColor,
-                )
-              },
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-              },
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Nesco Car Garage",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: Text(
+                            " We provide precision auto repair and maintenance services, ensuring the highest standard of quality and safety for your vehicle. Our experienced mechanics specialize in servicing a variety of makes and models."),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
+          ),
+        ],
+      ),
     );
   }
 }
